d6095320613f59aca45dfcbc2edda8a2d776750c
7777a2333 <603484945@qq.com>
2023-01-26 13:36:50 +0800

    添加fanguaijfh

--- README.md
+++ README.md
@@ -1,21 +1,26 @@
 # 随意更改FabricMod
 
 ## 用处?
+
 更多实用方块,更多合成!还有一些已经被删除的成就!
 
 ## 前置?
+
 JEI
 [下载(CurseForge)](https://www.curseforge.com/minecraft/mc-mods/jei)
 
 Fabric API
 
 ## 如何安装?
+
 将前置和mod放进MODS文件夹
 
 ## 许可证
+
 采用GNU GPL v3协议分发
 
 ## 其他
+
 默认Gradle配置文件使用了BMCLAPI和MCBBS作为下载源
 Gradle里导入了JEI
 数据包部分使用了https://misode.github.io/辅助
--- src/main/java/github/_7777a2333_/suiyigenggai/SuiYiGengGaiMain.java
+++ src/main/java/github/_7777a2333_/suiyigenggai/SuiYiGengGaiMain.java
@@ -1,16 +1,24 @@
 package github._7777a2333_.suiyigenggai;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.Material;
+import net.minecraft.item.BlockItem;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class SuiYiGengGaiMain implements ModInitializer {
-	// 写入日志的Mod名称
+	// 鍐欏叆鏃ュ織鐨凪od鍚嶇О
 	public static final Logger LOGGER = LoggerFactory.getLogger("SuiYiGengGai");
+	public static final Block PuLuShi = new Block(FabricBlockSettings.of(Material.METAL).strength(4.0f).requiresTool());
 
 	@Override
 	public void onInitialize() {
-		// Mod初始化完毕时运行的代码
-
+		Registry.register(Registry.BLOCK, new Identifier("suiyigenggai", "pu_lu_shi"), PuLuShi);
+		Registry.register(Registry.ITEM, new Identifier("suiyigenggai", "pu_lu_shi"), new BlockItem(PuLuShi, new FabricItemSettings()));
 	}
 }
